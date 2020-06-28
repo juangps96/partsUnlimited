19f1b65740d725253de14f4e38e76223ab03eec9
Juan Gabriel Peñaherrera Sandoval <juan.penaherrera@grupobabel.com>
2020-06-28 13:15:09 -0500

    subido

---
+++ src/PartsUnlimitedWebsite/Startup.cs
@@ -0,0 +1,21 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Owin;
+using Owin;
+using PartsUnlimited;
+using System.Web.Configuration;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+//comment
+namespace PartsUnlimited
+{
+	// bellevue comment!!
+	// second commit
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
